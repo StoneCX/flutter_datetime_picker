c383e9a4fa1b69abcda914e124e6169711f32568
chengxc <chengxcv5@163.com>
2022-10-09 20:25:36 +0800

    add birthday model to keep ui consistent

--- lib/src/date_model.dart
+++ lib/src/date_model.dart
@@ -975,3 +975,274 @@ class DateWithPerminentPickerModel extends CommonPickerModel {
     return currentTime;
   }
 }
+
+class DateBirthDayPickerModel extends CommonPickerModel {
+  late DateTime maxTime;
+  late DateTime minTime;
+
+  int currentSelectLeftIndex = 0;
+  DateBirthDayPickerModel(
+      {DateTime? currentTime,
+      DateTime? maxTime,
+      required DateTime minTime,
+      LocaleType? locale})
+      : super(locale: locale) {
+    this.maxTime = maxTime ?? DateTime(2049, 12, 31);
+    this.minTime = DateTime(minTime.year - 1, minTime.month, minTime.day);
+
+    currentTime = currentTime ?? DateTime.now();
+
+    if (currentTime.compareTo(this.maxTime) > 0) {
+      currentTime = this.maxTime;
+    } else if (currentTime.compareTo(this.minTime) < 0) {
+      currentTime = this.minTime;
+    }
+
+    this.currentTime = currentTime;
+
+    _fillLeftLists();
+    _fillMiddleLists();
+    _fillRightLists();
+    int minMonth = _minMonthOfCurrentYear();
+    int minDay = _minDayOfCurrentMonth();
+
+    /// 初始化 leftIndex index值
+    _currentLeftIndex = this.currentTime.year - this.minTime.year;
+    // _currentLeftIndex = 0;
+
+    /// 初始化 middleIndex index值
+    _currentMiddleIndex = this.currentTime.month - minMonth;
+    // _currentMiddleIndex = 0;
+
+    /// 初始化 githtIndex index值
+    _currentRightIndex = this.currentTime.day - minDay;
+    // _currentRightIndex = 0;
+  }
+
+  /// 填充 left list 数据
+  void _fillLeftLists() {
+    this.leftList = List.generate(maxTime.year - minTime.year, (int index) {
+      // print('LEFT LIST... ${minTime.year + index}${_localeYear()}');
+
+      return '${minTime.year + index}${_localeYear()}';
+    });
+  }
+
+  /// 当前年份最大月
+  int _maxMonthOfCurrentYear() {
+    return currentTime.year == maxTime.year ? maxTime.month : 12;
+  }
+
+  /// 当前年份最小月
+  int _minMonthOfCurrentYear() {
+    return currentTime.year == minTime.year ? minTime.month : 1;
+  }
+
+  /// 当前月最大日
+  int _maxDayOfCurrentMonth() {
+    int dayCount = calcDateCount(currentTime.year, currentTime.month);
+    return currentTime.year == maxTime.year &&
+            currentTime.month == maxTime.month
+        ? maxTime.day
+        : dayCount;
+  }
+
+  /// 当前月最小日
+  int _minDayOfCurrentMonth() {
+    return currentTime.year == minTime.year &&
+            currentTime.month == minTime.month
+        ? minTime.day
+        : 1;
+  }
+
+  /// 填充 middle list 数据
+  void _fillMiddleLists() {
+    int minMonth = _minMonthOfCurrentYear();
+    int maxMonth = _maxMonthOfCurrentYear();
+
+    this.middleList = List.generate(maxMonth - minMonth + 1, (int index) {
+      return '${_localeMonth(minMonth + index)}';
+    });
+  }
+
+  /// 填充 right list 数据
+  void _fillRightLists() {
+    int maxDay = _maxDayOfCurrentMonth();
+    int minDay = _minDayOfCurrentMonth();
+
+    this.rightList = List.generate(maxDay - minDay + 1, (int index) {
+      // if (_currentLeftIndex == 0) {
+      // print("in fillRightList left index is ${minDay + index}${_localeDay()}");
+      // }
+
+      return '${minDay + index}${_localeDay()}';
+    });
+  }
+
+  /// 当前选中的 left index value
+  @override
+  void setLeftIndex(int index) {
+    super.setLeftIndex(index);
+    // print("in setLeftIndex this value is:$index");
+    //adjust middle
+    currentSelectLeftIndex = index;
+    int destYear = index + minTime.year;
+    int minMonth = _minMonthOfCurrentYear();
+    DateTime newTime;
+    //change date time
+    if (currentTime.month == 2 && currentTime.day == 29) {
+      newTime = currentTime.isUtc
+          ? DateTime.utc(
+              destYear,
+              currentTime.month,
+              calcDateCount(destYear, 2),
+            )
+          : DateTime(
+              destYear,
+              currentTime.month,
+              calcDateCount(destYear, 2),
+            );
+    } else {
+      newTime = currentTime.isUtc
+          ? DateTime.utc(
+              destYear,
+              currentTime.month,
+              currentTime.day,
+            )
+          : DateTime(
+              destYear,
+              currentTime.month,
+              currentTime.day,
+            );
+    }
+    //min/max check
+    if (newTime.isAfter(maxTime)) {
+      currentTime = maxTime;
+    } else if (newTime.isBefore(minTime)) {
+      currentTime = minTime;
+    } else {
+      currentTime = newTime;
+    }
+
+    _fillMiddleLists();
+    _fillRightLists();
+    minMonth = _minMonthOfCurrentYear();
+    int minDay = _minDayOfCurrentMonth();
+    _currentMiddleIndex = currentTime.month - minMonth;
+    _currentRightIndex = currentTime.day - minDay;
+  }
+
+  /// 当前选中的 middle index value
+  @override
+  void setMiddleIndex(int index) {
+    super.setMiddleIndex(index);
+    //adjust right
+    int minMonth = _minMonthOfCurrentYear();
+    int destMonth = minMonth + index;
+    DateTime newTime;
+    //change date time
+    int dayCount = calcDateCount(currentTime.year, destMonth);
+    newTime = currentTime.isUtc
+        ? DateTime.utc(
+            currentTime.year,
+            destMonth,
+            currentTime.day <= dayCount ? currentTime.day : dayCount,
+          )
+        : DateTime(
+            currentTime.year,
+            destMonth,
+            currentTime.day <= dayCount ? currentTime.day : dayCount,
+          );
+    //min/max check
+    if (newTime.isAfter(maxTime)) {
+      currentTime = maxTime;
+    } else if (newTime.isBefore(minTime)) {
+      currentTime = minTime;
+    } else {
+      currentTime = newTime;
+    }
+
+    _fillRightLists();
+    int minDay = _minDayOfCurrentMonth();
+    _currentRightIndex = currentTime.day - minDay;
+  }
+
+  /// 当前选中的 right index value
+  @override
+  void setRightIndex(int index) {
+    super.setRightIndex(index);
+    print("in setRightIndex this value is:$index");
+    int minDay = _minDayOfCurrentMonth();
+    currentTime = currentTime.isUtc
+        ? DateTime.utc(
+            currentTime.year,
+            currentTime.month,
+            minDay + index,
+          )
+        : DateTime(
+            currentTime.year,
+            currentTime.month,
+            minDay + index,
+          );
+  }
+
+  @override
+  String? leftStringAtIndex(int index) {
+    if (index >= 0 && index < leftList.length) {
+      return leftList[index];
+    }
+    return null;
+  }
+
+  @override
+  String? middleStringAtIndex(int index) {
+    if (index >= 0 && index < middleList.length) {
+      return middleList[index];
+    }
+    return null;
+  }
+
+  @override
+  String? rightStringAtIndex(int index) {
+    if (index >= 0 && index < rightList.length) {
+      return rightList[index];
+    }
+    return null;
+  }
+
+  String _localeYear() {
+    if (locale == LocaleType.zh || locale == LocaleType.jp) {
+      return '年';
+    } else if (locale == LocaleType.ko) {
+      return '년';
+    } else {
+      return '';
+    }
+  }
+
+  String _localeMonth(int month) {
+    if (locale == LocaleType.zh || locale == LocaleType.jp) {
+      return '$month月';
+    } else if (locale == LocaleType.ko) {
+      return '$month월';
+    } else {
+      List monthStrings = i18nObjInLocale(locale)['monthLong'] as List<String>;
+      return monthStrings[month - 1];
+    }
+  }
+
+  String _localeDay() {
+    if (locale == LocaleType.zh || locale == LocaleType.jp) {
+      return '日';
+    } else if (locale == LocaleType.ko) {
+      return '일';
+    } else {
+      return '';
+    }
+  }
+
+  @override
+  DateTime finalTime() {
+    return currentTime;
+  }
+}
